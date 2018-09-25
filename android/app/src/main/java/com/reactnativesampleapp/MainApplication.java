b21453b28220a4b8f1d1c54daced820477b3b07c
jaypinedabengco <jaybengcodeveloper@gmail.com>
2018-09-25 17:05:14 +0800

    ADD signature

--- android/app/src/main/java/com/reactnativesampleapp/MainApplication.java
+++ android/app/src/main/java/com/reactnativesampleapp/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.CallbackManager;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.rssignaturecapture.RSSignatureCapturePackage;
 import org.wonday.pdf.RCTPdfView;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RSSignatureCapturePackage(),
             new RCTPdfView(),
             new RNFetchBlobPackage(),
           new FBSDKPackage(mCallbackManager)
--- src/screens/PDFMakeDemoScreen.js
+++ src/screens/PDFMakeDemoScreen.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
-import { View, Text, Button, Dimensions, StyleSheet } from 'react-native'
+import { View, Text, Image, Button, Dimensions, StyleSheet } from 'react-native'
 import documentDefinition from './../sample-data/documentDefinition/entry-notice-form-9.json'
 import pdfMake from 'pdfmake/build/pdfmake.js'
 import 'pdfmake/build/vfs_fonts'
 import Pdf from 'react-native-pdf'
-
+import SignatureCapture from 'react-native-signature-capture'
 class PDFMakeDemoScreen extends Component {
   constructor(prop) {
     super(prop)
     this.state = {
       documentDefinition: documentDefinition,
+      savedSignature: null,
       base64Pdf: null
     }
   }
@@ -30,13 +31,37 @@ class PDFMakeDemoScreen extends Component {
     }
   }
 
+  _onSaveSignatureEvent(result) {
+    // save base64 image
+    this.setState({ savedSignature: `data:image/png;base64,${result.encoded}` })
+  }
+
   render() {
     const source = { uri: this.state.base64Pdf }
+    const { savedSignature } = this.state
     return (
       <View style={styles.container}>
-        <Text>PDFMake Demo</Text>
         {this.state.base64Pdf === null ? (
-          <Button title="build pdf" onPress={() => this._buildPDF()} />
+          <View>
+            <SignatureCapture
+              style={styles.signature}
+              onSaveEvent={result => this._onSaveSignatureEvent(result)}
+              viewMode={'portrait'}
+              saveImageFileInExtStorage={false}
+              showTitleLabel={false}
+            />
+            <View style={styles.preview}>
+              {savedSignature ? (
+                <Image
+                  style={styles.previewSignature}
+                  source={{ uri: savedSignature }}
+                />
+              ) : (
+                <Text>No Saved Signature</Text>
+              )}
+              <Button title="build pdf" onPress={() => this._buildPDF()} />
+            </View>
+          </View>
         ) : (
           <View>
             <Button
@@ -60,6 +85,16 @@ const styles = StyleSheet.create({
   pdf: {
     flex: 1,
     width: Dimensions.get('screen').width
+  },
+  preview: {
+    flex: 2
+  },
+  signature: {
+    flex: 1
+  },
+  previewSignature: {
+    flex: 1,
+    alignItems: 'center'
   }
 })
 
